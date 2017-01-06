8d2806e8848764b52965727dc75064d8c7002626
Hongta <wuht@sph.com.sg>
2017-01-06 17:34:51 +0800

    working on traverse
    fixed insert bugs
    working on traverse with yield

--- tree/README.md
+++ tree/README.md
@@ -3,6 +3,12 @@
 
 ### Binary Search Tree
 
+  * insert : use the following algorithm, to insert a key `k`
+    - If the tree is empty, construct a leaf node with key = `k`
+    - If `k` is equal to the key of root node, overwrite the `payload`
+    - If `k` is less then the key of root node, insert it into the left child
+    - If `k` is greater than the key of root node, insert it into the right child
+
 ### Tries
 
 
--- tree/binary_search_tree.py
+++ tree/binary_search_tree.py
@@ -7,31 +7,45 @@ class BinarySearchTree:
     def __init__(self):
         self._root = None;
 
-    def insert(self, key, payload=None):
-        #1 tree is empty construct the tree
+    def insert(self, k, payload=None):
+        # tree is empty construct the tree
         if not self._root:
-            self._root(Node(key,payload))
+            self._root= TreeNode(k,payload)
         else:
-            self._insert(self, self._root, key, payload)
+            self._insert(self._root, k, payload)
 
-    def _insert(self, tree_node, key, payload=None):
-        n = TreeNode(key, payload)
-        if key < tree_node.key:
+    def _insert(self, tree_node, k, payload=None):
+        n = TreeNode(k, payload)
+        if k == tree_node.key:
+            tree_node.payload = payload
+            return
+
+        if k < tree_node.key:
             if not tree_node.left:
-                tree_node.right = n
+                tree_node.left = n
             else:
-                self._insert(self, tree_node.left, key, payload)
+                self._insert(tree_node.left, k, payload)
         else:
             if not tree_node.right:
                 tree_node.right = n
             else:
-                self._insert(self, tree_node.right, key, payload)
+                self._insert(tree_node.right, k, payload)
 
     def delete(self, v):
         pass
 
-    def traverse(self, callback):
-        pass
+    def traverse(self):
+        self._traverse(self._root)
+
+    def _traverse(self, node):
+        if node:
+            if node.left:
+                for n in _traverse(node.left):
+                    yield n
+            yield node
+            if node.right:
+                for n in self._traverse(node.right):
+                    yield n
 
     def height(self):
         pass
@@ -41,3 +55,17 @@ class BinarySearchTree:
 
     def count():
         pass
+
+if __name__ == "__main__":
+    t = BinarySearchTree()
+    t.insert(3)
+    t.insert(8)
+    t.insert(12)
+    t.insert(1)
+    t.insert(15)
+    t.insert(7)
+    print(t._root.right.left)
+    # p = list(t.traverse())
+    # p
+    # for v in p:
+    #     print v.key
