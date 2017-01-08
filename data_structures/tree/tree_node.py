366da021d86466c8aa8389f6cfe80386172c3b8f
Hongta Wu <hongta.wu@gmail.com>
2017-01-08 11:48:17 +0800

    Add set_children function in TreeNode

--- data_structures/tree/tree_node.py
+++ data_structures/tree/tree_node.py
@@ -13,3 +13,24 @@ class TreeNode(object):
         if self.payload:
             s += ": " + str(self.payload)
         return s
+
+    def set_children(self, **kwargs):
+        """
+            This funciton used to set node's children, and also update its parent
+            Usage:
+            node1.set_children(left=node2)
+            node1.set_children(right=node3)
+            node1.set_children(left=node2, right=node3)
+        """
+        old_children = (self.left, self.right)
+        if "left" in kwargs:
+            self.left = kwargs["left"]
+            if self.left:
+                self.left.parent = self
+
+        if "right" in kwargs:
+            self.right = kwargs["right"]
+            if self.right:
+                self.right.parent = self
+
+        return old_children
