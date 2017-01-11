23f0dc0f900beb7b8f885124e1b30b29db42ccf4
Hongta <wuht@sph.com.sg>
2017-01-11 15:29:22 +0800

    implemented AVLTree insertion
    fixed parent missing issue
    removed get_max_height function

--- data_structures/tree/avl_tree.py
+++ data_structures/tree/avl_tree.py
@@ -6,28 +6,28 @@ from binary_search_tree import BinarySearchTree
 
 class AVLTree(BinarySearchTree):
     def __init__(self):
-        pass
+        super(AVLTree, self).__init__()
 
     def insert(self, key):
         if not self._root:
             self._root = AVLTreeNode(key)
         else:
-            self._root = self._insert(key, self._root)
+            self._root = self._insert(self._root, key)
 
     def _insert(self, node, key):
         if not node:
             return AVLTreeNode(key)
 
         if key < node.key:
-            node.left = self._insert(node.left, key)
+            node.set_children(left=self._insert(node.left, key))
         elif key > node.key:
-            node.right = self._insert(node.right, key)
+            node.set_children(right=self._insert(node.right, key))
         else:
             # duplicated key
             return node
 
         # 2. update height of the ancestor node
-        node.height = get_max_height(node.left, node.right) + 1
+        node.height = max(self.height(node.left), self.height(node.right)) + 1
 
         # 3. check whether the node became unbalanced
         balance = self.get_balance(node)
@@ -50,23 +50,10 @@ class AVLTree(BinarySearchTree):
         else:
             return n.height
 
-    def get_balance(node):
+    def get_balance(self, node):
         if not node:
             return 0
         return self.height(node.left) - self.height(node.right);
-
-    def get_max_height(self, x, y):
-        if x is None:
-            x_height = 0
-        else:
-            x_height = x.height
-
-        if y is None:
-            y_height = 0
-        else:
-            y_height = y.height
-
-        return max(x_height, y_height)
 
     def _right_rotate(self, node):
         k1 = node.left
@@ -76,8 +63,8 @@ class AVLTree(BinarySearchTree):
         node.set_children(left=k1.right)
         k1.set_children(right=node)
 
-        node.height = self.get_max_height(node.right, node.left) + 1
-        k1.height = self.get_max_height(k1.left, k1.right) + 1
+        node.height = max(self.height(node.right), self.height(node.left)) + 1
+        k1.height = max(self.height(k1.left), self.height(k1.right)) + 1
 
         return k1
 
@@ -88,7 +75,21 @@ class AVLTree(BinarySearchTree):
         node.set_children(right=k2.left)
         k2.set_children(left=node)
 
-        node.height = self.get_max_height(node.left, node.right) + 1
-        k2.height = self.get_max_height(k2.left, k2.right) + 1
+        node.height = max(self.height(node.left), self.height(node.right)) + 1
+        k2.height = max(self.height(k2.left), self.height(k2.right)) + 1
 
         return k2
+
+if __name__ == '__main__':
+    t = AVLTree()
+    t.insert(10)
+    t.insert(15)
+    t.insert(20)
+    t.insert(25)
+    t.insert(30)
+    p = t.search(20)
+    print p, p.left, p.right, p.height, p.parent
+    p = t.search(15)
+    print p, p.left, p.right, p.height, p.parent
+    p = t.search(25)
+    print p, p.left, p.right, p.height, p.parent
