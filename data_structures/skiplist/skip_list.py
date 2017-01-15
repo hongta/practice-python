d24f9c77e7c50194142f702f30e648fd3cb0e5c7
Hongta Wu <hongta.wu@gmail.com>
2017-01-15 10:54:16 +0800

    Rewrite skip list

--- data_structures/skiplist/skip_list.py
+++ data_structures/skiplist/skip_list.py
@@ -2,31 +2,89 @@
 # -*- coding: utf-8 -*-
 
 import random
-from itertools import dropwhile, count, cycle
-from skip_node import SkipNode, NIL
+from skip_node import SkipNode
 
 class SkipList(object):
 
     def __init__(self):
-        self._head = None
-        self._tail = SkipNode(NIL())
-        self._height = 10
-        pass
+        self.height = 3
+        self._head = SkipNode(None, self.height, None)
+        self.count = 0
+
+    @property
+    def head(self):
+        return self._head
+
+
+    def _update_list(self, key):
+        update = [None] * self.height
+        node = self.head
+
+        for i in reversed(range(len(self.head.next))):
+            while node.next[i] != None and node.next[i].key < key:
+                node = node.next[i]
+            update[i] = node
+
+        return update
+
+
+    def search (self, key):
+        return self._search(key)
+
+    def _search(self, key, update):
+        if update == None:
+            update = self._update_list(key)
+
+        if len(update) > 0:
+            candidate = update[0].next[0]
+            if candidate and candidate.key == key:
+                return candidate
+        return None
+
+    def insert(self, key, payload=None):
+        node = SkipNode(key, self._pick_level())
+
+        #adjust maximum height of list
+        self.height = max(self.height, len(node.next))
+        while len(self.head.next) < len(node.next):
+            self.head.next.append(None)
+
+        update = self._update_list(key)
+        node_found = self._search(key, update)
+        if node_found == None:
+            for i in range(len(node.next)):
+                node.next[i] = update[i].next[i]
+                update[i].next[i] = node
+            self.count += 1
+        else:
+            #if node exists, replace payload
+            node_found.payload = payload
+
+    def delete(self, key):
+        update = self._update_list(key)
+        node = self._search(key, update)
+        if node:
+            for i in range(len(node.next)):
+                update[i].next[i] = node.next[i]
+
+                #adjust list height
+                if self.head.next[i] == None:
+                    self.height -= 1
+            self.count -= 1
+
+    def print_list(self):
+        for i in range(len(self.head.next)-1, -1, -1):
+            print "lvl ",i, ": "
+            x = self.head
+            lst = []
+            while x.next[i] != None:
+                print x.next[i].key,
+                x = x.next[i]
+                print ''
+
 
     def _pick_level(self):
         lvl = 1
-        while random.random() < 0.5 and lvl < self._height:
+        while random.random() < 0.5:
             lvl += 1
         return lvl
-
-
-    def _geometric(self, p):
-        return (next(dropwhile(lambda _: random.randint(1, int(1. / p)) == 1, count())) for _ in cycle([1]))
-
-
-if __name__ == '__main__':
-    s = SkipList()
-    print s._pick_level()
-    print s._pick_level()
-    print s._pick_level()
-    print s._pick_level()
--- data_structures/skiplist/skip_node.py
+++ data_structures/skiplist/skip_node.py
@@ -2,13 +2,17 @@
 # -*- coding: utf-8 -*-
 
 class SkipNode(object):
-    __slots__ = ('key', 'payload', 'next', 'down', 'height')
+    __slots__ = ('key', 'payload', 'next', 'level', 'height')
 
-    def __init__(self, key, payload=None):
+    def __init__(self, key, level=1, payload=None):
         self.key = key
         self.payload = payload
-        
+        self.next = [None] * level
+
         pass
+
+    def __str__(self):
+        return "SkipNode(%s)" % self.key
 
 class NIL(object):
 
