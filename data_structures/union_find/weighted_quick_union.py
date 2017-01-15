844aff45eb1804b461460368f97af4f73a6b62f0
Hongta Wu <hongta.wu@gmail.com>
2017-01-15 17:01:28 +0800

    Fix quick union functions issue
    Missing counter
    Find function should return position of element
    Decrement counter in union

--- data_structures/union_find/weighted_quick_union.py
+++ data_structures/union_find/weighted_quick_union.py
@@ -4,16 +4,20 @@
 
 class WeightedQuickUnion(object):
 
-    def __init__(self):
-        self.id = []
-        self.weight = []
+    def __init__(self, data=None):
+        self.id = data
+        self.weight = [1] * len(data)
+        self.count = len(data)
+
+    def count(self):
+        return self.count
 
     def find(self, val):
         p = val
         while self.id[p] != p:
             p = self.id[p]
 
-        return self.id[p]
+        return p
 
     def union(self, p, q):
         p_root = self.find(p)
@@ -23,6 +27,7 @@ class WeightedQuickUnion(object):
             return
 
         self.id[q_root] = p_root
+        self.count -= 1
 
     def is_connected(self, p, q):
         return self.find(p) == self.find(q)
