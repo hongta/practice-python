514d92f5b7eef3397406adaef7ff39342fc031ab
Hongta Wu <hongta.wu@gmail.com>
2017-01-15 11:36:36 +0800

    Add skip list unittest

---
+++ data_structures/tests/test_skip_list.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from random import Random
+import sys
+import os
+sys.path.append(os.path.join('..', 'data_structures'))
+from skiplist.skip_list import SkipList
+from skiplist.skip_node import SkipNode
+
+
+class TestSkipList(unittest.TestCase):
+
+    def setUp(self):
+        self.sl = SkipList()
+
+    def test_empty(self):
+        self.assertEqual(self.sl.count, 0)
+        self.assertEqual(self.sl.height, 3)
+        for i in self.sl.head.next:
+            self.assertEqual(i, None)
+
+    # Test Insertion
+
+    def test_insert_in_asc_order(self):
+        self.sl.insert(1)
+        self.sl.insert(2)
+        self.sl.insert(3)
+        self.sl.insert(4)
+        p = self.sl.head.next[0]
+        self.assertEqual(p.key, 1)
+        p = p.next[0]
+        self.assertEqual(p.key, 2)
+        p = p.next[0]
+        self.assertEqual(p.key, 3)
+        p = p.next[0]
+        self.assertEqual(p.key, 4)
+        if self.sl.head.next[1]:
+            self.assertIn(self.sl.head.next[1].key, [1,2,3,4])
+        if self.sl.head.next[2]:
+            self.assertIn(self.sl.head.next[1].key, [1,2,3,4])
+
+    def test_insert_in_desc_order(self):
+        self.sl.insert(4)
+        self.sl.insert(3)
+        self.sl.insert(2)
+        self.sl.insert(1)
+        p = self.sl.head.next[0]
+        self.assertEqual(p.key, 1)
+        p = p.next[0]
+        self.assertEqual(p.key, 2)
+        p = p.next[0]
+        self.assertEqual(p.key, 3)
+        p = p.next[0]
+        self.assertEqual(p.key, 4)
+        if self.sl.head.next[1]:
+            self.assertIn(self.sl.head.next[1].key, [1,2,3,4])
+
+        if self.sl.head.next[2]:
+            self.assertIn(self.sl.head.next[1].key, [1,2,3,4])
+
+    def test_insert_in_unorder(self):
+        self.sl.insert(1)
+        self.sl.insert(3)
+        self.sl.insert(4)
+        self.sl.insert(2)
+        p = self.sl.head.next[0]
+        self.assertEqual(p.key, 1)
+        p = p.next[0]
+        self.assertEqual(p.key, 2)
+        p = p.next[0]
+        self.assertEqual(p.key, 3)
+        p = p.next[0]
+        self.assertEqual(p.key, 4)
+        if self.sl.head.next[1]:
+            self.assertIn(self.sl.head.next[1].key, [1,2,3,4])
+        if self.sl.head.next[2]:
+            self.assertIn(self.sl.head.next[1].key, [1,2,3,4])
+
+    def test_insert_same_key(self):
+        self.sl.insert(1)
+        self.sl.insert(3)
+        self.sl.insert(4)
+        self.sl.insert(2)
+        self.sl.insert(3)
+        self.assertEqual(self.sl.count, 4)
+        p = self.sl.head.next[0]
+        self.assertEqual(p.key, 1)
+        p = p.next[0]
+        self.assertEqual(p.key, 2)
+        p = p.next[0]
+        self.assertEqual(p.key, 3)
+        p = p.next[0]
+        self.assertEqual(p.key, 4)
+        if self.sl.head.next[1]:
+            self.assertIn(self.sl.head.next[1].key, [1,2,3,4])
+        if self.sl.head.next[2]:
+            self.assertIn(self.sl.head.next[1].key, [1,2,3,4])
+
+    # Test Deletion
+
+    def test_delete_in_list(self):
+        self.sl.insert(1)
+        self.sl.insert(3)
+        self.sl.insert(4)
+        self.assertEqual(self.sl.count, 3)
+        self.sl.delete(1)
+        self.assertEqual(self.sl.count, 2)
+
+    def test_delete_not_in_list(self):
+        self.sl.insert(1)
+        self.sl.insert(3)
+        self.sl.insert(4)
+        self.assertEqual(self.sl.count, 3)
+        self.sl.delete(5)
+        self.assertEqual(self.sl.count, 3)
+
+    def tearDown(self):
+        self.sl = None
+
+
+# if __name__ == '__main__':
+#     unittest.main()
