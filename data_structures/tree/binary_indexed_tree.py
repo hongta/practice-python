eba0a94aa3e786c5dbe9a132b0021308cbb5fb44
Hongta <wuht@sph.com.sg>
2017-01-22 23:08:46 +0800

    implemented Binary Indexed Tree

--- data_structures/tree/binary_indexed_tree.py
+++ data_structures/tree/binary_indexed_tree.py
@@ -21,6 +21,13 @@ class BinaryIndexedTree(object):
 
         self._array = data
 
+    def sum(self, i):
+        r = 0
+        while i:
+            r += self._array[i-1]
+            i -= self._lowbit(i)
+        return r
+
     def range(self, i, j):
         """
         Returens the sum of elements [i, j-1]
@@ -36,11 +43,20 @@ class BinaryIndexedTree(object):
             i -= self._lowbit(i)
         return r
 
+    def add(self, i, delta):
+
+        while i < len(self._array):
+            self._array[i] += delta
+            i += self._lowbit(i+1)
+
     def get(self, i):
         """
         Returns the value of the element at index i
         """
         return self.range(i, i+1)
+
+    def set(self, i, val):
+        self.add(i, val - self.get(i))
 
 
 if __name__ == '__main__':
@@ -48,4 +64,6 @@ if __name__ == '__main__':
     t._build([1,2,3,4,5,6,7,8,9])
     print t.range(2,5)
     print t.get(3)
+    print t._array
+    t.set(3, 9)
     print t._array
