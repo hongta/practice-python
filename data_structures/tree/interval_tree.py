9cc00488aa5a79d571eadc32501b749d3930ce44
Hongta <wuht@sph.com.sg>
2017-01-24 23:16:27 +0800

    update search function in Interval Tree

--- data_structures/tree/interval_tree.py
+++ data_structures/tree/interval_tree.py
@@ -35,11 +35,27 @@ class IntervalTree(object):
         return False
 
     def search(self, interval):
+        return self._search(self.root, interval)
+
+    def _search(self, node, interval):
+
+        if not node:
+            return None
+
+        print "is overlaps", self.is_overlap((node.low, node.high), interval)
+        if self.is_overlap((node.low, node.high), interval):
+            return (node.low, node.high)
+
+        if node.left and node.left.max >= interval[0]:
+            return self._search(node.left, interval)
+
+        return self._search(node.right, interval)
+
 
 if __name__ == '__main__':
     t = IntervalTree()
     t.insert((5,12))
     t.insert((4,8))
     t.insert((10,15))
-    
+    print t.root
     print t.search((13,15))
