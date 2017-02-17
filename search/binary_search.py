b4241323a52c18f80f1e3ab47a64bf9fa32abccd
Hongta <wuht@sph.com.sg>
2017-02-17 13:55:09 +0800

    add non-recursive version of Binary Search

--- search/binary_search.py
+++ search/binary_search.py
@@ -17,7 +17,21 @@ def binary_search(a, needle):
 
     return _binary_search(0, len(a)-1)
 
+def binary_search2(a, needle):
+    lo = 0
+    hi = len(a) - 1
+    while lo <=hi:
+        mid = lo + (hi - lo) // 2
+        if a[mid] < needle:
+            lo = mid + 1
+        elif a[mid] > needle:
+            hi = mid - 1
+        else:
+            return mid
+    return False
+
+
 if __name__ == '__main__':
     d = sorted([2,3,6, 21, 23,5, 10,30,24, 15])
     print d
-    print binary_search(d, 10)
+    print binary_search2(d, 10)
