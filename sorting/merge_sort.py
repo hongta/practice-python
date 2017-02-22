047059091ffb54b6df526cadc558ee801f4d2b9c
Hongta <wuht@sph.com.sg>
2017-02-22 18:08:51 +0800

    add two more Merge Sorting function

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -30,6 +30,36 @@ def merge_sort(a):
 
     return result
 
+def merge_sort2(a):
+    return _merget_sort2(a, 0, len(a))
+
+def _merge_sort2(a, lo, hi):
+    if hi - lo > 1:
+        mid = lo + (hi - lo) // 2
+        _merget_sort2(a, lo, mid)
+        _merget_sort2(a, mid, hi)
+        _merge2(a, ho, mid, hi)
+    return a
+
+def _merge2(a, lo, mid, hi):
+    i = 0
+    j = mid
+    #TODO: will update this function
+
+from heapq import merge
+
+def _merge3(a):
+    if len(a) < 2:
+        return a
+        
+    mid = len(a) // 2
+    return merge(merge3(a[:mid]), merge3(a[mid:]))
+
+def merge3(a):
+    return list(_merge3(a))
+
+
 if __name__ == '__main__':
     d = [34,2,24,12, 45,33,9,99]
-    print merge_sort(d)
+    #print merge_sort(d)
+    print merge3(d)
