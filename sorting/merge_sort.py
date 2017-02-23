44ae6725bb30ddc6197a68e33b49398b7665b45a
Hongta <wuht@sph.com.sg>
2017-02-23 10:32:32 +0800

    woring on another Merge Sorting function

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -31,15 +31,17 @@ def merge_sort(a):
     return result
 
 def merge_sort2(a):
-    return _merget_sort2(a, 0, len(a))
+    _merge_sort2(a, 0, len(a)-1)
+    return a
 
 def _merge_sort2(a, lo, hi):
-    if hi - lo > 1:
-        mid = lo + (hi - lo) // 2
-        _merget_sort2(a, lo, mid)
-        _merget_sort2(a, mid, hi)
-        _merge2(a, ho, mid, hi)
-    return a
+    if hi <= lo:
+        return
+    mid = lo + (hi - lo) // 2
+    _merge_sort2(a, lo, mid)
+    _merge_sort2(a, mid+1, hi)
+    _merge(a, lo, mid, hi)
+
 
 def _merge2(a, lo, mid, hi):
     i = 0
@@ -48,18 +50,49 @@ def _merge2(a, lo, mid, hi):
 
 from heapq import merge
 
-def _merge3(a):
+def _merge_sort3(a):
     if len(a) < 2:
         return a
-        
+
     mid = len(a) // 2
     return merge(merge3(a[:mid]), merge3(a[mid:]))
 
-def merge3(a):
-    return list(_merge3(a))
+def merge_sort3(a):
+    return list(_merge_sort3(a))
+
+def _merge(a, lo, mid, hi):
+    i = lo
+    j = mid + 1
+    aux = a[:]
+    print lo,mid,hi
+    for k in range(lo, hi + 1):
+        if i > mid:
+            a[k] = aux[j]
+            j+=1
+        elif j > hi+1:
+            a[k] = aux[i]
+            i +=1
+        elif aux[j] < aux[i]:
+            a[k] = aux[j]
+            j+=1
+        else:
+            a[k] = aux[i]
+            i+=1
+
+def merge_sort4(a):
+    N = len(a)
+    sz = 1
+    for sz in range(1, N, sz+sz):
+        print sz
+        for lo in range(0,N-sz, sz+sz):
+            print lo
+            _merge(a, lo, lo+sz-1, min(lo+sz+sz-1,N-1))
+
+    return a
+
 
 
 if __name__ == '__main__':
     d = [34,2,24,12, 45,33,9,99]
     #print merge_sort(d)
-    print merge3(d)
+    print merge_sort2(d)
