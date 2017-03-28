a3820cc52e2bf0b08ee67ab665f8e4f5eb6049bc
Hongta <wuht@sph.com.sg>
2017-03-28 16:34:24 +0800

    updated optimized Knapsack problem

--- algorithms/knapsack.py
+++ algorithms/knapsack.py
@@ -6,10 +6,7 @@ def knapsack(W, w, v, n):
     F = [[0] * (W+1) for _ in range(n+1)]
     for i in range(n):
         for j in range(W+1):
-            # if i == 0:
-            #     F[i][j] == 0
-            #     break
-            if W >= w[i]:
+            if j >= w[i]:
                 F[i][j] = max(F[i-1][j], F[i-1][j-w[i]]+v[i])
             else:
                 F[i][j] = F[i-1][j]
@@ -21,11 +18,13 @@ def knapsack2(W, w, v, n):
 
     for i in range(n):
         for j in range(W, w[i], -1):
-            F[j] = max(F[j-w[i]])
+            F[j] = max(F[j-w[i]] + v[i], F[j])
+    return F[-1]
 
 if __name__ == '__main__':
-    W = 10
-    w = [2,2,6,5,4]
-    v = [6,3,5,4,6]
-    n = 5
-    print knapsack(W, w, v, n)
+    W = 16
+    w = [4,5,10,3]
+    v = [30,20,40,10]
+    n = 4
+    print(knapsack(W, w, v, n))
+    print(knapsack2(W, w, v, n))
